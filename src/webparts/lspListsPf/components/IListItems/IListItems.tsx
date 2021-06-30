73053602ef57ed05bcc860a24e8c35fa1f5e3139
Maya-Mostafa <mayamostafa@gmail.com>
2021-06-30 11:00:00 -0400

    items details list component

--- src/webparts/lspListsPf/components/IListItems/IListItems.tsx
+++ src/webparts/lspListsPf/components/IListItems/IListItems.tsx
@@ -1,18 +1,60 @@
 import * as React from 'react';
 import styles from '../LspListsPf.module.scss';
 import { IListItemsProps } from './IListItemsProps';
-import {DetailsList, PrimaryButton} from '@fluentui/react';
+import {DetailsList, IColumn, SelectionMode, DetailsListLayoutMode, initializeIcons, Icon, Persona, PersonaSize, PersonaPresence} from '@fluentui/react';
 
 
 export default function IListItems (props: IListItemsProps){
 
-    let listItems = props.listItems;
+    const columns: IColumn[] = [
+        {
+          key: 'column1',
+          name: 'fullName',
+          minWidth: 80,
+          maxWidth: 200,
+          onRender : (item: any) => (
+            <div>{item.lastName}, {item.firstName}</div>
+          )
+        },
+        {
+            key: 'column4',
+            name: 'pNumber',
+            minWidth: 80,
+            maxWidth: 200,
+            onRender : (item: any) => (
+              <div>{item.empNo}</div>
+            )
+        },
+        {
+          key: 'column2',
+          name: 'jobTitleLocs',
+          minWidth: 80,
+          maxWidth: 300,
+          onRender : (item: any) => (
+            <div>
+              <div>{item.jobTitle}</div>
+              <div>{item.locNames}</div>
+            </div>
+          )
+        },
+        {
+          key: 'column3',
+          name: 'email',
+          minWidth: 80,
+          maxWidth: 80,
+          onRender : (item: any) => (
+            <a href={`mailto:${item.email}`} target="_blank">{item.email}</a>
+          )
+        },
+    ];
 
     return(
-        <div>
-            <DetailsList 
-                items={listItems}
-            />
-        </div>
+        <DetailsList
+            items={props.listItems}
+            columns={columns}
+            isHeaderVisible={false}
+            selectionMode={SelectionMode.none}
+            layoutMode={DetailsListLayoutMode.justified}
+        />
     );
 }
